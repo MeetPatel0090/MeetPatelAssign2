86bd32dd912bb06e349e429b9ce70fee4bf2fed7
Meet2304 <meetpatel12302@gmail.com>
2022-06-06 19:03:28 -0400

    Made some changes to the menu and added the rent screen(not functioning at this point).

--- MeetAssign22/src/main/java/meet/patel/n01460090/PatelActivity.java
+++ MeetAssign22/src/main/java/meet/patel/n01460090/PatelActivity.java
@@ -5,7 +5,10 @@ package meet.patel.n01460090;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PatelActivity extends MenuActivity {
 
@@ -13,5 +16,15 @@ public class PatelActivity extends MenuActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patel);
+
+        Button btn = findViewById(R.id.MeetButton);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(PatelActivity.this, RentActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
---
+++ MeetAssign22/src/main/java/meet/patel/n01460090/RentActivity.java
@@ -0,0 +1,14 @@
+package meet.patel.n01460090;
+
+
+import android.os.Bundle;
+
+public class RentActivity extends MenuActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rent);
+
+    }
+}
